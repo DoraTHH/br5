d649fc0c9369cea604dcd2d3eea788da0edcaef7
DoraTHH <erikerikssonsepost@gmail.com>
2024-10-07 14:19:31 +0200

    added marios question

--- solution.c
+++ solution.c
@@ -79,3 +79,31 @@ int question_d () {
     }
         return correct;
     };
+int questionM()
+{
+    printf("Which burek shown bellow is the best one");
+    printf("\n1. Cheese"
+           "\n2. Meat"
+           "\n3. Spinach");
+    printf("\nBest burek: ");
+    int inp=0;
+    int ret=0;
+    scanf("%d" ,&inp);
+    switch (inp)
+    {
+    case 1:
+        ret=0;
+        break;
+    case 2:
+        ret=1;
+        break;
+    case 3:
+        ret=0;
+        break;
+    
+    default:
+        printf("\nJebo ti majku choose an existing one ");
+        break;
+    }
+    return ret;
+};
